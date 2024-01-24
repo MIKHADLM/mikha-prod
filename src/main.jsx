2b81bdb7ba783d6f58c92f0f1f35701a33c6b778
n65vcm <liam.le-strat@insee.fr>
2024-01-24 16:07:50 +0100

    fix pipeline

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { ThemeProvider, createTheme } from "@mui/material";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.jsx";
