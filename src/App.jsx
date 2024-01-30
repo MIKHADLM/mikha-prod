4d7d8d99cc94e4f7e2c83ebcf6d285af0b7e953a
n65vcm <liam.le-strat@insee.fr>
2024-01-30 13:34:47 +0100

    name titre

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { Outlet } from "react-router-dom";
 import "./index.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = "Mikha Prod - Portfolio";
+  }, []);
   return (
     <>
       <nav></nav>
