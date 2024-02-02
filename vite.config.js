5653adb3de98e36172dcd6547fa801dfa1c68354
n65vcm <liam.le-strat@insee.fr>
2024-02-02 13:24:18 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import legacy from "vite-plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), legacy()],
+  plugins: [react()],
   base: "/mikha-prod/",
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
