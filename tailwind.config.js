d6e6179d25338868446220b9f83e27b71dcd960d
MIKHADLM <158492386+MIKHADLM@users.noreply.github.com>
2025-01-26 12:19:35 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{jsx, tsx}', 'public/index.html'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Inclut tous les fichiers JS, JSX, TS et TSX dans le dossier `src`
+    "./public/index.html"         // Assure que le fichier HTML principal est pris en compte
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
