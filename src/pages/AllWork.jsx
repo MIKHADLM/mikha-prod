0c031003feb90cb6feeb5cc7b7bd915064d12492
MIKHADLM <158492386+MIKHADLM@users.noreply.github.com>
2025-01-26 14:23:37 +0100

    Update AllWork.jsx

--- src/pages/AllWork.jsx
+++ src/pages/AllWork.jsx
@@ -69,12 +69,12 @@ const AllWork = () => {
                   className="object-cover h-full w-full group-hover:opacity-25 group-focus:opacity-25 transition-opacity duration-300"
                 />
                 {/* Texte associé à la miniature */}
-                <span className="absolute bottom-1 left-1 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 group-hover:transition-opacity">
+                <span className="absolute bottom-1 left-1 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 group-hover:transition-opacity font-semibold">
                   {video.date.split("\n").map((line, index) => (
-                    <React.Fragment key={index}>
-                      {line}
-                      <br />
-                    </React.Fragment>
+                  <React.Fragment key={index}>
+                  {line}
+                  <br />
+                  </React.Fragment>
                   ))}
                 </span>
               </div>
