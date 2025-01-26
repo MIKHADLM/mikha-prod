b8647efce2f1e84aca63d28bf5f9f567919a194d
MIKHADLM <158492386+MIKHADLM@users.noreply.github.com>
2025-01-26 19:16:05 +0100

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,15 +11,7 @@ const NavBar = () => {
         <Link to="/" className="text-2xl text-white font-bold">
           MIKHA
         </Link>
-
-        {/* Menu Hamburger (visible uniquement sur mobile) */}
-        <button
-          className="lg:hidden text-white text-2xl focus:outline-none"
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
-        >
-          ☰
-        </button>
-
+       
         {/* Liens de navigation */}
         <div
           className={`lg:flex items-center space-x-10 absolute lg:static top-16 left-0 w-full lg:w-auto bg-black lg:bg-transparent z-50 transition-transform duration-300 ${
@@ -37,12 +29,6 @@ const NavBar = () => {
             className="block lg:inline-block text-lg text-white hover:font-bold px-4 py-2 lg:py-0"
           >
             À PROPOS
-          </Link>
-          <Link
-            to="/contact/"
-            className="block lg:inline-block text-lg text-white hover:font-bold px-4 py-2 lg:py-0"
-          >
-            CONTACT
           </Link>
         </div>
       </div>
