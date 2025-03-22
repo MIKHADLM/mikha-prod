88d67c91ffb6dd8666928e76341b448b8e7682ab
MIKHADLM <158492386+MIKHADLM@users.noreply.github.com>
2025-03-22 16:10:41 +0100

    Mise à jour de la page About et correction de la NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 const NavBar = () => {
   const location = useLocation(); // Récupère l'URL actuelle
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      setVisible(currentScrollPos < prevScrollPos || currentScrollPos < 10);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos]);
+
+  // 🔥 Remonte en haut quand la page change
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [location.pathname]);
 
   return (
-    <nav className="fixed top-0 w-full z-50 backdrop-blur-sm p-4 lg:p-8">
+    <nav
+      className={`fixed top-0 w-full z-50 backdrop-blur-sm p-4 lg:p-8 transition-transform duration-300 ${
+        visible ? "translate-y-0" : "-translate-y-full"
+      }`}
+    >
       <div className="mx-auto flex justify-between items-center">
         <Link to="/" className="text-2xl text-white font-bold">
           MIKHA
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,7 @@ import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import VolumeUpOutlinedIcon from "@mui/icons-material/VolumeUpOutlined";
 import EnfantInternet from "../assets/Enfant_Internet.mp3";
+import Photo_mikha_1 from "../assets/Photo_mikha_1.jpg";
 
 const About = () => {
   function playAudio() {
@@ -11,30 +12,40 @@ const About = () => {
   }
 
   return (
-    <div className="bg-black min-h-screen overflow-x-hidden pt-14 px-0">
+    <div className="bg-black min-h-screen overflow-x-hidden pt-14 px-0 flex flex-col items-center">
       <NavBar />
-      <div className="flex flex-col p-8 flex-grow">
-        <div>
-          <div className="text-4xl font-bold text-white pt-5">
-            MICHAEL PHILIBERT
+      <div className="flex flex-col p-8 flex-grow items-center text-center w-full">
+        <div className="flex flex-col items-center">
+          <div className="text-xl font-bold text-white pt-5">
+            Réalisateur, vidéaste, cadreur/monteur et fondateur de Mehomies
           </div>
-          <div className="text-xl text-white pb-5">
-            RÉALISATEUR / DA / CADREUR / MONTEUR / VIDÉASTE FREELANCE
+          <div className="text-6xl text-white pb-5">
+            Salut ! Moi, c'est Mikha.
           </div>
+          
           <div className="text-xl text-white pb-8">
             <span className="text-[#00FF00] text-2xl pr-3 font-extrabold">
               ●
             </span>
             DISPONIBLE POUR TOUS PROJETS
-          </div>
-          <div className="text-xl text-white pb-1 font-semibold">Contact</div>
+          </div> 
+          {/* Centrage de l'image */}
+          <img src={Photo_mikha_1} alt="À propos" className="w-[70vw] max-w-none rounded-lg shadow-lg mx-auto" />
+          
+          <div className="text-xl text-white pb-1 font-semibold"> {/* Contact */} </div>
           <div className="text-lg text-white font-thin">
+            {/*
             <MailOutlineIcon /> contact@mikhaprod.com
+            */}
           </div>
-          <div className="text-lg text-white pb-5 font-thin">
+          <div 
+            className="text-lg text-white pb-5 font-thin">
+              {/*
             <InstagramIcon /> mikha_dlm
+            */}
           </div>
-          <div className="text-xl text-white font-semibold inline">
+          <div  className="text-xl text-white font-semibold inline">
+            {/*
             Enfant d'internet{" "}
             <span className="text-xs">
               (expression){" "}
@@ -43,28 +54,46 @@ const About = () => {
                 className="cursor-pointer mb-2"
               />
             </span>
-          </div>
+            */}
+          </div> 
           <audio id="audiomp3" src={EnfantInternet} type="audio/mp3"></audio>
-          <div className="text-lg text-white pb-5 font-thin  lg:w-1/2">
-            Personne ayant été exposée assez jeune à Internet et ayant développé
-            une grande partie de ses compétences grâce à des outils numériques.
+          <div  className="text-lg text-white pb-5 font-thin lg:w-1/2">
+            {/*
+             Personne ayant été exposée assez jeune à Internet et ayant développé
+             une grande partie de ses compétences grâce à des outils numériques. 
+            */}
           </div>
-          <div className="text-xl text-white pb-1 font-semibold">
+        
+          <div className="text-xl text-white pb-1 font-semibold text-left">
             Mon parcours
           </div>
-          <div className="text-lg text-white pb-5 font-thin lg:w-1/2">
-            Passionné par la culture Hip-Hop, j'ai lancé en 2019 le projet DLM -
-            DEEP IN CULTURE, un média Rap basé sur Rennes. De là, j'ai commencé
-            à filmer des concerts, des festivals, des clips mais j'i surtout
-            découvert une nouvelle passion. Je me suis formé de projet en
-            projet, cherchant à m'améliorer constamment grâce à Internet et dans
-            le cadre de mes études.
+          <div className="text-lg text-white pb-5 font-thin lg:w-1/2 text-left text-justify">
+            <br/>
+            Basé à Lille, j'accompagne depuis 2019 des artistes et des entreprises de tous horizons dans la mise en valeur de leur vision afin d’engager leur audience. <br/>
+            <br/>
+            C’est par passion pour la création et avec l’envie d’entreprendre que je suis devenu vidéaste freelance à 19 ans. D’abord en parallèle de mes études, puis à plein temps depuis novembre 2023. <br/>
+            <br/>
+            Grâce à mon expérience dans la réalisation de clips et mes collaborations avec des entreprises aux secteurs variés, j’ai développé une grande polyvalence. Aujourd’hui, je peux vous accompagner de A à Z, de l'idée initiale jusqu'à la rencontre avec votre public.
           </div>
-          <div className="text-xl text-white pb-1 font-semibold">Diplômes</div>
-          <div className="text-lg text-white pb-5 font-thin pl-5">
+
+          <div className="text-xl text-white pb-1 font-semibold text-left">
+            Derrière mais aussi devant la caméra ! 
+          </div>
+          <div className="text-lg text-white pb-5 font-thin lg:w-1/2 text-left text-justify">
+            <br/>
+              Depuis 2024, je prends énormément de plaisir à créer du contenu, que ce soit pour mes propres réseaux ou pour le média Hip-Hop RapMinute. Cette activité me permet d'expérimenter différents formats et d'affiner ma vision de la création digitale.
+            <br/>
+            <br/>
+            Avec plus d'un million de vues cumulées sur mes vidéos, j'ai beaucoup appris sur l'importance du storytelling ainsi que sur l'impact d'un montage qualitatif et percutant. Fort de cette expérience, j'accompagne aujourd'hui créateurs de contenu et entrepreneurs dans leur communication.
+            <br/>
+          </div>
+
+          <div className="text-xl text-white pb-1 font-semibold text-left">Diplômes</div>
+          {/* Suppression de pl-5 pour bien centrer la liste */}
+          <div className="text-lg text-white pb-5 font-thin">
             <li className="lg:w-1/2">
               Détenteur d'une{" "}
-              <b>Licence Arts du spectacle - Etudes cinématographiques</b>
+              <b>Licence Arts du spectacle - Études cinématographiques</b>
             </li>
             <li className="lg:w-1/2">
               Détenteur d'une{" "}
