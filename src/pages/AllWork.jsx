5207cac5c9ee3ed66c3ac31141084c96e0918834
MIKHADLM <158492386+MIKHADLM@users.noreply.github.com>
2026-01-05 13:18:54 +0100

    fix: force autoplay on mobile modal using onReady and playsinline

--- src/pages/AllWork.jsx
+++ src/pages/AllWork.jsx
@@ -446,9 +446,17 @@ const AllWork = () => {
                 opts={{
                   width: '100%',
                   height: '100%',
-                  playerVars: { autoplay: 1 }
+                  playerVars: {
+                    autoplay: 1,
+                    playsinline: 1,
+                    modestbranding: 1,
+                    rel: 0
+                  }
                 }}
                 className="w-full h-full"
+                onReady={(e) => {
+                  e.target.playVideo();
+                }}
               />
             )}
           </Box>
